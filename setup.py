de149d4d27d75ee1a1e71580a8559f1138e21386
Annamalai <Annamalai@course5i.com>
2023-10-17 18:24:12 +0530

    staging branch added

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-This file configures the Python package with entrypoints used for future runs on Databricks.
+This file configures the Python package with entrypoints used for future runs on Databricks..
 
 Please follow the `entry_points` documentation for more details on how to configure the entrypoint:
 * https://setuptools.pypa.io/en/latest/userguide/entry_point.html
